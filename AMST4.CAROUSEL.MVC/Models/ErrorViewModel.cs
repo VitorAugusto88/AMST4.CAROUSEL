365840f56dbd0f9c80fa3ee607a585a3aa22e204
VitorAugusto88 <visilvavidal@gmail.com>
2024-04-15 18:32:24 -0300

    commit

---
+++ AMST4.CAROUSEL.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AMST4.CAROUSEL.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
